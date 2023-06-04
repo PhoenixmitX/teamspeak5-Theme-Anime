4eb7dee9cc8cecd573538155e4cf81abb9808301
PhoenixmitX <hauke.weinert@gmail.com>
2023-06-04 23:47:21 +0200

    remove unnecessary line from build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cp $(ls styles/**/*.{gif,png,css}) build/de.wargamer.anime.teamspeak/
 
 # merge json files
 jq -s '.' $(find ./styles/**/* | grep json) > build/themes.json
-jq -s '.[0] + (.content.themes = .[1])' styles/package.json build/themes.json > build/de.wargamer.anime.teamspeak/package.json
 jq -c --slurpfile themes build/themes.json '.content.themes |= $themes[0]' styles/package.json > build/de.wargamer.anime.teamspeak/package.json
 
 # zip files
